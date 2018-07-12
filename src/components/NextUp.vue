568e163f478779c6e67a6a0069f83fead1b8f399
Andrew Westwick <andrewwestwick@iheartmedia.com>
2018-07-12 13:38:13 -0500

    add raters

--- src/components/NextUp.vue
+++ src/components/NextUp.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="next-up-wrapper">
-    <h2>Next Up</h2>
     <div class="next-tracks-container">
       <div class="next-track" v-for="track in $store.state.nextUp">
         <div class="next-title">{{ track.title }}</div>
-        <div class="next-artist">{{ track.artist }} &sdot; {{track.album}}</div>
+        <div class="next-artist">{{ track.artist }} &middot; {{track.album}}</div>
+        <div class="next-up-rater">
+          <div class="rater vote-down">
+            <i class="far fa-thumbs-down"></i>
+            <span class="down-votes">3</span>
+          </div>
+          <div class="rater vote-up">
+            <i class="far fa-thumbs-up"></i>
+            <span class="up-votes">1</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -20,16 +29,36 @@ export default {
 .next-up-wrapper {
   text-align: left;
   padding: 0 16px;
-  margin-top: 16px;
 
   h2 {
     font-weight: bold;
+    margin-bottom: 8px;
   }
 }
 
 .next-track {
-  border-bottom: 1px solid #ccc;
-  padding: 8px 0;
+  border-bottom: 1px solid #eee;
+  padding: 8px 80px 8px 0;
+  position: relative;
+}
+
+.next-up-rater {
+  position: absolute;
+  right: 0;
+  top: 12px;
+  display: flex;
+  .rater {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-left: 16px;
+    color: #bbb;
+    .up-votes,.down-votes {
+      font-size: 12px;
+      color: #bbb;
+    }
+  }
 }
 
 .next-title {
