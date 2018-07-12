b9104366d8fc43840210bb49b9c6cc47da89e7be
Andrew Westwick <andrewwestwick@iheartmedia.com>
2018-07-12 13:23:17 -0500

    add nextup

--- src/components/NextUp.vue
+++ src/components/NextUp.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="next-up-wrapper">
     <h2>Next Up</h2>
+    <div class="next-tracks-container">
+      <div class="next-track" v-for="track in $store.state.nextUp">
+        <div class="next-title">{{ track.title }}</div>
+        <div class="next-artist">{{ track.artist }} &sdot; {{track.album}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +21,32 @@ export default {
   text-align: left;
   padding: 0 16px;
   margin-top: 16px;
+
+  h2 {
+    font-weight: bold;
+  }
+}
+
+.next-track {
+  border-bottom: 1px solid #ccc;
+  padding: 8px 0;
+}
+
+.next-title {
+  font-size: 14px;
+  font-weight: 500;
+  color: #333;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.next-artist {
+  font-size: 13px;
+  font-weight: 400;
+  color: #666;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -12,7 +12,12 @@ export default new Vuex.Store({
       artist: 'Michael Jackson',
       title: 'Thriller',
       album: 'Thriller'
-    }
+    },
+    nextUp: [
+      {artist: 'Dr. Dre', title: 'The Next Episode', album: '2001'},
+      {artist: 'Kanye West Feat. JAY Z & Big Sean', title: 'Clique', album: 'Cruel Summer'},
+      {artist: 'C+C Music Factory', title: 'Gonna Make You Sweat (Everybody Dance Now)', album: 'Gonna Make You Sweat'}
+    ]
   },
   mutations: {
     setNick(state, nickname) {
