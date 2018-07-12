365393f3728e2786a0774612c3669ec0836652fb
Andrew Westwick <andrewwestwick@iheartmedia.com>
2018-07-12 13:54:11 -0500

    add searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search-bar">
+    <div class="control has-icons-left">
+      <input type="text" class="input" placeholder="Search by Artist, Track, or Album" />
+      <span class="icon is-small is-left">
+        <i class="fas fa-search"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+}
+</script>
+
+<style lang="scss">
+.search-bar {
+  width: 100vw;
+  background: #C50930;
+  padding: 16px;
+  background: linear-gradient(to bottom, #C50930, #A60628);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,15 +2,18 @@
   <div class="mainapp">
     <now-playing></now-playing>
     <next-up></next-up>
+
+    <search-bar></search-bar>
   </div>
 </template>
 
 <script>
 import NowPlaying from '../components/NowPlaying'
 import NextUp from '../components/NextUp'
+import SearchBar from '../components/SearchBar'
 
 export default {
-  name: 'party',
-  components: {NowPlaying, NextUp}
+  name: 'searchbar',
+  components: { NowPlaying, NextUp, SearchBar }
 }
 </script>
